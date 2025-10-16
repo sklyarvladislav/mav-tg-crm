0bed681c2a936c2f47780c194ac8bb2e45f35521
JohnFrankl1n <john@DESKTOP-1NINMJ6.localdomain>
2025-10-16 19:48:44 +0300

    Add_project_table

---
+++ src/api/infra/database/tables/projects.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Integer, String, Text, TIMESTAMP, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import mapped_column
+from datetime import datetime
+import uuid
+
+from src.api.infra.database.tables.base import registry
+
+
+@registry.mapped_as_dataclass(kw_only=True)
+class Projects:
+    __tablename__ = "projects"
+
+    project_id: uuid.UUID = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    name: str = mapped_column(String(100), nullable=False)
+    description: str | None = mapped_column(Text)
+    status: str | None = mapped_column(String(50))
+    owner: int | None = mapped_column(ForeignKey("users.user_id"))
+    created_by: int | None = mapped_column(ForeignKey("users.user_id"))
+    created_at: datetime = mapped_column(
+        TIMESTAMP, default=datetime.utcnow, nullable=False
+    )
+
+
+@registry.mapped_as_dataclass(kw_only=True)
+class ProjectParticipants:
+    __tablename__ = "project_participants"
+
+    project_id: uuid.UUID = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("projects.project_id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    user_id: int = mapped_column(
+        Integer,
+        ForeignKey("users.user_id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    role: str | None = mapped_column(String(50))
+    added_at: datetime = mapped_column(
+        TIMESTAMP, default=datetime.utcnow, nullable=False
+    )
